5f043153ea58a6aa8011236cfdc2efebbea72f2c
luweiwen <luweiwen@haodf.com>
2019-01-14 18:03:28 +0800

    首页切图

---
+++ src/page/home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+  <header>
+  <nav class="main-menu menu-fixed">
+	<div class="container">
+	  <!-- Brand and toggle get grouped for better mobile display -->
+	  <div class="navbar-header">
+		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		  <span class="sr-only">Toggle navigation</span>
+		  <span class="icon-bar"></span>
+		  <span class="icon-bar"></span>
+		  <span class="icon-bar"></span>
+		</button>
+		<a class="navbar-brand" href="index.html"><img src="../assets/img/logo.png" alt="logo" class="img-responsive"></a>
+	  </div>
+
+	  <!-- Collect the nav links, forms, and other content for toggling -->
+	  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		
+		<ul class="nav navbar-nav navbar-right">
+		  <li><a class="page-scroll" href="#banner">Home</a></li>
+		  <li><a class="page-scroll" href="#about">About</a></li>
+		  <li><a class="page-scroll" href="#services">Services</a></li>
+		  <li><a class="page-scroll" href="#skills">Skills</a></li>
+		  <li><a class="page-scroll" href="#portfolio">Portfolio</a></li>
+		  <li><a class="page-scroll" href="#team">Team</a></li>
+		  <li><a class="page-scroll" href="#pricing">Pricing</a></li>
+		  <li><a class="page-scroll" href="#testimonial">Testimonial</a></li>
+		  <li><a class="page-scroll" href="#blog">Blog</a></li>
+		  <li><a class="page-scroll" href="#client">Client</a></li>
+		  <li><a class="page-scroll" href="#contact">Contact</a></li>
+		</ul>
+
+	  </div><!-- /.navbar-collapse -->
+	</div><!-- /.container -->
+  </nav>
+</header>
+<section id="banner" class="banner">
+  <div class="overlay">
+	<div class="banner-content">
+	  <h3>Welcome to Our World</h3>
+	  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+	  <a href="#about" class="default-button page-scroll">Get Started</a>
+	</div><!-- banner content -->
+  </div><!-- overlay -->
+</section>
+
+  </div>
+</template>
+<script>
+export default {
+  name: "home"
+};
+</script>
+<style>
+
+
+</style>
